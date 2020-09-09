f53f80b1f83d1a6ee048c225c844efeeeb495d42
serviktor050 <mr.s-gagulin@yandex.ru>
2020-09-09 16:09:42 +0300

    Добавлено решение задачи

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,25 @@
-const gameField = document.querySelector('.game-field');
+const gameFieldCell = document.querySelectorAll('.game-field-cell');
 
-console.dir(gameField);
+const getRandomInt = (min, max) => {
+  const minimal = Math.ceil(min);
+  const maximal = Math.floor(max);
+  return Math.floor(Math.random() * (maximal - minimal + 1)) + minimal;
+};
+
+window.addEventListener('load', () => {
+  const randomInt = getRandomInt(1, gameFieldCell.length);
+  gameFieldCell[randomInt].innerHTML = '<img src = "../src/img/goblin.png">';
+});
+
+setInterval(() => {
+  let targetIndex;
+  do {
+    targetIndex = getRandomInt(1, 16);
+
+    for (let i = 0; i <= 15; i += 1) {
+      gameFieldCell[i].innerHTML = '';
+    }
+
+    gameFieldCell[targetIndex].innerHTML = '<img src = "../src/img/goblin.png">';
+  } while (targetIndex === 0);
+}, 1000);
